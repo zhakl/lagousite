a2f2ad24c6741af670a2b2f4e389a4e734ae8ce4
zkl <15999556296@163.com>
2017-06-23 13:55:16 +0800

    add

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import MinUi from 'mint-ui'
+import axios from 'axios'
 import VueRouter from 'vue-router'
 import 'mint-ui/lib/style.css'
 Vue.use(MinUi)
@@ -9,13 +10,18 @@ Vue.use(VueRouter)
 import Enroll from './views/Enroll.vue'
 import Register from './views/Register.vue'
 const router = new VueRouter({
+  // mode: 'history', //切换路径模式，变成history模式
+  scrollBehavior: () => ({ y: 0 }), // 滚动条滚动的行为，不加这个默认就会记忆原来滚动条的位置
   routes: [
     {path: '/', component: Enroll, name: 'enroll'},
     {path: '/enroll', component: Enroll, name: 'enroll'},
     {path: '/register', component: Register, name: 'register'},
     {path: '*', redirect: '/'}
   ]
-});
+})
+Vue.prototype.$http = axios  //其他页面在使用axios的时候直接  this.$http就可以了
+axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
+axios.defaults.baseURL = 'http://localhost:3000/';
 new Vue({
   el: '#app',
   router,
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,83 +1,66 @@
 <template>
     <div class="wrap">
-        <mt-field  placeholder="请输入用户名" class="username"   v-model="username"></mt-field>
-        <mt-field  placeholder="手机号" type="email" class="email" v-model="email"></mt-field>
-        <mt-field  placeholder="设置6-16位密码" type="text" class="password" v-model="password">
-            <!--<i class="Eye" :class="{'EyeClosed':Eye,'EyeOpen':!Eye}"@click="toggle"></i>-->
+        <mt-field placeholder="请输入用户名" class="username" v-model="username"></mt-field>
+        <mt-field placeholder="手机号" type="email" class="email" v-model="phone"></mt-field>
+        <mt-field placeholder="设置6-16位密码" type="password" class="password" v-model="password">
+            <i></i>
         </mt-field>
-        <mt-button size="large" class="enroll">登录</mt-button>
-        <p class="noAccount">还没有账号？</p>
         <mt-button size="large" class="register" @click.stop="storeAddressInfo()">注册</mt-button>
-        <p class="noAccount">点击注册，即代表您同意 <a href="">《拉钩用户协议》</a></p>
+        <p class="noAccount">点击注册，即代表您同意 <a>《拉钩用户协议》</a></p>
     </div>
 </template>
 <script>
-  export default{
-    name:'enroll',
-    data(){
-      return{
-        username:'',
-        email:'',
-        password:'',
-//        Eye:false
-      }
-    },
-    methods: {
-//      toggle: function () {
-//        this.Eye = !this.Eye;
-//      },
-      storeAddressInfo () {
-//        debugger
-        var self = this;
-        if (!self.username) {
-          alert("请填写用户名")
-          return false;
+    import { MessageBox } from 'mint-ui';
+    export default{
+        name: 'enroll',
+        data(){
+            return {
+                username: '',
+                phone: '',
+                password: '',
+            }
+        },
+        methods: {
+            storeAddressInfo () {
+                var self = this;
+                if(!/^[a-zA-Z0-9_]{6,16}$/.test(self.username)){
+                    MessageBox('提示', '用户名为6到16的数字或者汉字');
+                }
+                else if (!/^1[34578]\d{9}$/.test(self.phone)) {
+                    MessageBox('提示', '手机号码不正确或者为空')
+                    return false;
+                }
+                else if (!/^\d{6,20}$/.test(self.password)) {
+                    MessageBox('提示', '密码数字为6到16位')
+                    return false;
+                } else {
+                    this.fetchData();
+                }
+            },
+            fetchData(){
+                var self = this;
+                self.$http.post('http://localhost:3000/register', {
+                        username: this.username,
+                        phone: this.phone,
+                        password: this.password
+                    }
+                ).then(function (res) {
+                    console.log(res.data)
+                    if(res.data.status){
+                        if(res.data.data.success == '0'){
+                            MessageBox('提示', '该手机号码已经被注册')
+                        }else{
+                            MessageBox.alert('注册成功').then(action => {
+                                self.$router.push({name: "enroll"});
+                            });
+                        }
+                    }
+                }).catch(function (err) {
+                });
+            }
         }
-        if  (!/^1[34578]\d{9}$/.test(self.email)) {
-          alert("手机号码不正确或者为空")
-          return false;
-        }
-        if (!/^\d{6,20}$/.test(self.password)) {
-          alert("密码数字为6到16位")
-          return false;
-        }
-      }
-
-//        else {
-//          if (!self.address['Mobile'] || !/^1[34578]\d{9}$/.test(self.address['Mobile'])) {
-//            self.$vux.toast.show({
-//              'text': "手机号为空或者手机号码不正确",
-//              'type': 'warn',
-//              'width': '55%'
-//            })
-//            return false;
-//          }
-
-//        }
-//
-        // console.log(self.addressInfo);
-
-//        this.saveAddressInfo(self.address);
-      },
-
-
-  }
+    }
 </script>
 <style>
     @import '../assets/index.css';
-    .email{
-        border-radius: 0;
-    }
-    .Eye{
-        background: url("../assets/img/psweye_930f545.png") 0 0 no-repeat;
-        width: 50px;
-        height: 25px;
-        display: inline-block;
-    }
-    .eyeOpen{
-        background: url("../assets/img/psweye_930f545.png") 0 -32px no-repeat;
-        width: 50px;
-        height: 25px;
-        display: inline-block;
-    }
 </style>
