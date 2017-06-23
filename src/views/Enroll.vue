d6f93c319148b0bfb0aab4b0bad05e624d8a60ed
liangjianmin <jianmin.liang@dolabank.com>
2017-06-23 16:06:20 +0800

    css

--- src/views/Enroll.vue
+++ src/views/Enroll.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="wrap">
-        <mt-field placeholder="请输入手机号或者用户名" type="text" class="username" v-model="username"></mt-field>
-        <mt-field placeholder="请输入密码" type="password" class="password" v-model="password"></mt-field>
-        <mt-button size="large" class="enroll" @click.stop="login()">登录</mt-button>
+        <mt-field  placeholder="已验证手机/邮箱" type="email" class="email" v-model="email"></mt-field>
+        <mt-field  placeholder="密码" type="password" class="password" v-model="password"></mt-field>
+        <mt-button size="large" class="enroll">登录</mt-button>
         <p class="noAccount">还没有账号？</p>
         <router-link :to="{ 'name':'register'}" class="register-but">
             <mt-button size="large" class="register">注册</mt-button>
@@ -11,33 +11,13 @@
 </template>
 <script>
     export default{
-        name: 'enroll',
-        data(){
-            return {
-                username: '',
-                password: ''
-            }
-        },
-        methods:{
-            login(){
-                var self = this;
-                self.$http.post('http://localhost:3000/login', {
-                        username: this.username,
-                        password: this.password
-                    }
-                ).then(function (res) {
-                    if (res.data.status) {
-                       if(res.data.data.success){
-                           console.log(res.data.data.msg)
-                           self.$http.post('http://localhost:3000/session').then(function (data) {
-                               console.log(data)
-                           });
-                       }
-                    }
-                }).catch(function (err) {
-                });
-            }
+      name:'enroll',
+      data(){
+        return{
+          email:'',
+          password:''
         }
+      }
     }
 </script>
 <style>
